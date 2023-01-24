79caa1ee8439e282e475057fa63045724d29586c
AariyJP <aariyjp@aariy.net>
2023-01-24 12:09:21 +0900

    RandomWordBot1

--- src/main/java/net/aariy/Main.java
+++ src/main/java/net/aariy/Main.java
@@ -32,7 +32,7 @@ public class Main extends ListenerAdapter
                 Commands.slash("起動1", "ランダムに文章と画像を表示します。"),
                 Commands.slash("add", "メッセージと画像を追加します。")
                         .addOption(OptionType.STRING, "メッセージ", "送信するメッセージ内容", true)
-                        .addOption(OptionType.STRING, "画像url", "", true)
+                        .addOption(OptionType.STRING, "画像url", "送信する画像へのリンク", true)
         ).queue();
         jda.addEventListener(new Main());
     }
@@ -44,8 +44,8 @@ public class Main extends ListenerAdapter
         {
             e.deferReply().queue();
             SecureRandom sc = new SecureRandom();
-            String target = sc.nextInt(node.size())+"";
-            String[] res = node.get(target).asText().split("\\|");
+            int a = sc.nextInt(node.size());
+            String[] res = node.get(a).asText().split("\\|");
             e.reply(res[0]).queue();
             e.getChannel().sendMessage(res[1]).queue();
             /*
@@ -74,7 +74,7 @@ public class Main extends ListenerAdapter
         }
         if(e.getName().equals("add"))
         {
-            node.put(""+node.size()+1, "%s|%s".formatted(e.getOption("メッセージ"), e.getOption("画像url")));
+            node.put(""+node.size()+1, "%s|%s".formatted(e.getOption("メッセージ").getAsString(), e.getOption("画像url").getAsString()));
             reload();
         }
     }
